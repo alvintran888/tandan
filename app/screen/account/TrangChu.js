b5ae9f1e9bd152e03bbc29b461f494e68f054559
nghia <you@example.com>
2022-11-08 00:02:57 +0700

    toithuhai

--- app/screen/account/TrangChu.js
+++ app/screen/account/TrangChu.js
@@ -12,7 +12,6 @@ const TrangChu = () => {
     // cục màu xanh chứa th người
     <View style={styles.background}>
       <View style={{flex: 1, backgroundColor: '#3AC5C9', borderRadius: 30}}>
-
         {/* nút quay lại */}
         <View style={styles.return}>
           <TouchableOpacity
@@ -25,61 +24,56 @@ const TrangChu = () => {
 
         {/* chia phần trong cục màu xanh */}
         <View style={{flex: 8, flexDirection: 'row'}}>
-
           {/* ảnh th người */}
-        <View style={styles.human}>
+          <View style={styles.human}>
             <Image source={require('@app/assets/images/Human.png')} />
-        </View>
+          </View>
 
-            {/* ảnh chữ */}
-        <View style={styles.chu}>
+          {/* ảnh chữ */}
+          <View style={styles.chutopbest}>
             <Image source={require('@app/assets/images/nhahang.png')} />
-        </View>
-
-
-
+          </View>
         </View>
       </View>
 
       <View style={{flex: 2}}>
         {/* ảnh đầu */}
-            <View style={{flexDirection:'row'}}>
-                
-                {/* ảnh */}
-                <View style={{flex:2, marginTop: 20, marginLeft:10,  }}>
-                    <Image source={require('../../assets/images/anhhai.png')}/> 
-                </View>
+        <View style={{flexDirection: 'row'}}>
+          {/* ảnh */}
+          <View style={{flex: 2, marginTop: 20, marginLeft: 10}}>
+            <Image source={require('../../assets/images/anhhai.png')} />
+          </View>
 
-                {/* chữ */}
-                <View style={{flex:4}}>
-                    <Text style={{marginTop:20,fontWeight: 'bold' , fontSize: 20, fontFamily: "Cochin" }}>LongHorn Steakhouse</Text>
-                    <Text style={{ fontSize: 20 }}>3605 Parker Rd.</Text>
-                </View>
+          {/* chữ */}
+          <View style={{flex: 4}}>
+            <Text style={styles.tennhahang}>LongHorn Steakhouse</Text>
+            <Text style={{fontSize: 17, marginTop: 5, color: "#3B3B3B"}}>3605 Parker Rd.</Text>
+
+            {/* đánh giá */}
+            <View style={{flexDirection:"row"}}>
+              <View style={styles.ngoisaodanhgia}>
+            <Image source={require('../../assets/images/ngoisao.png')} />
+            </View>
+
+            <View style={styles.sosaodanhgia}>
+            <Text style={{color: "#010101",fontSize:14}}>4.4</Text>
+            </View>
 
             </View>
+          </View>
+        </View>
 
         {/* ảnh hai */}
-            <View style={{flex:2}}>
-
-            </View>
+        <View style={{flex: 2}}></View>
 
         {/* ảnh ba */}
-            <View style={{flex:2}}>
-
-            </View>
+        <View style={{flex: 2}}></View>
 
         {/* ảnh bốn */}
-            <View style={{flex:2}}>
-
-            </View>
+        <View style={{flex: 2}}></View>
 
         {/* ảnh năm */}
-            <View style={{flex:2}}>
-
-            </View>
-
-
-        
+        <View style={{flex: 2}}></View>
       </View>
     </View>
   );
@@ -96,20 +90,48 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 10,
     marginTop: 10,
-    
+    marginTop: 30,
   },
   human: {
     flex: 4,
     height: 30,
     width: '100%',
+    marginLeft: 250,
   },
-  chu: {
+  chutopbest: {
     flex: 6,
-    marginTop: 40,
-    height:100,
+    marginTop: 45,
+    height: 100,
     width: 50,
-    color:"#FFFFFF"
+    color: '#FFFFFF',
+    position: 'absolute',
+    marginLeft: 20,
+  },
 
+  tennhahang: {
+    marginTop: 20,
+    fontSize: 20,
+    fontFamily: 'Helvetica Neue',
+    color: '#010101',
+    fontWeight: 'bold',
+  },
+
+  // chudiachi: {
+  //   fontSize: 17,
+  //   marginTop: 5,
+  //   color: ' #3B3B',
+  // },
+
+  ngoisaodanhgia: {
+    marginTop: 5,
+    flex: 0.7,
+    
+  },
+
+  sosaodanhgia:{
+    marginTop: 5,
+    flex:6,
+    
   },
 });
 // <View style={{flex: 1, backgroundColor: '#FFF'}}>
